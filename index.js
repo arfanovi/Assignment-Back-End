536e4a6c6fa1502b26a77f756d9404b78389e30f
arfanovi <arfanovi.gub1@gmail.com>
2024-08-24 15:38:56 +0600

    test server

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 
+const express = require('express');
+const app = express();
+const PORT = 3001
+
+
+
+app.get("/run", (req, res)=>{
+    res.send("Okay")
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port is ${PORT}`)
+})
